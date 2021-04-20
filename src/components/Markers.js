d617c74303b33d1715c0d4ce2f49e2f9895f00b0
ddimunzio <Tilos772>
2021-04-20 08:50:18 -0300

    -- Cambio la rutina del marker.js para que use la funcion map corectamente.

--- src/components/Markers.js
+++ src/components/Markers.js
@@ -1,30 +1,25 @@
 import React from 'react'
-import { Marker, Popup } from 'react-leaflet'
+import {Marker, Popup} from 'react-leaflet'
 import Icono from '../assets/Icon'
 import {venues} from '../assets/locations.json'
 
 const Marcador = () => {
-    let i = -1 //Si no hacía esta chanchada no me entraba en el json
-    const arrayMarkers = venues.map(() => {
-        i++
-        console.log(venues.[i].description)
-        return(
-        <Marker
-            key={i}
-            position={venues.[i].latlng}
-            icon={Icono}
-        >
-            <Popup>
-                {venues.[i].nombre}
-                <br/>
-                {venues.[i].description}
-                <br/>
-                {venues.[i].url}
-            </Popup>
-        </Marker>
-        )
-    })
-    return arrayMarkers
-    }
+  const arrayMarkers = venues.map((location) => {
+    return (
+      <Marker
+        position={location.latlng}
+        icon={Icono}>
+        <Popup>
+          {location.nombre}
+          <br/>
+          {location.description}
+          <br/>
+          {location.url}
+        </Popup>
+      </Marker>
+    )
+  })
+  return arrayMarkers
+}
 
 export default Marcador;
