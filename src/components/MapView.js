3a3e000deffa204588ecb77ac5bb958d57cfbb44
ddimunzio <Tilos772>
2021-04-20 20:26:38 -0300

    --Cambio mapa base

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -15,7 +15,7 @@ const MapView = () => {
             zoom="7"
         >
             <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                url="https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png"
                 attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
             />
             <Marcador />
